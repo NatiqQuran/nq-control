bc60a5c56836f5073192bca3dc4d990ec47c4857
AzerothA <telifesite@gmail.com>
2023-01-20 00:13:08 +0330

    panel button and page adde

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
-import { Main, Button } from "@yakad/ui";
+import { Main, Button, Page as Pg } from "@yakad/ui";
 
 export default function Page() {
     return (
-        <Main>
-            <Link href="/account/login">
-                <Button variant="filled">Login</Button>
-            </Link>
-        </Main>
+        <Pg style={{ position: "absolute", height: "100%" }}>
+            <Main>
+                <Link href="/account/login">
+                    <Button variant="filled">Login</Button>
+                </Link>
+
+                <Link href="/panel">
+                    <Button variant="filled">Open Panel</Button>
+                </Link>
+            </Main>
+        </Pg>
     );
 }
