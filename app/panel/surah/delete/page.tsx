a71917cea5c46ac025ff62dfe53ec700bfe72561
ahadvl <ahamech2@gmail.com>
2023-09-14 19:39:39 +0330

    add /surah dir

---
+++ app/panel/surah/delete/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { Button, Container, Row, Spacer } from "@yakad/ui";
+import { useRouter } from "next/navigation";
+
+export default function page() {
+  const router = useRouter();
+
+  return (
+    <Container>
+      <Row>
+        <h1>This page will be used for delete surah soon. </h1>
+        <Spacer />
+        <Button variant="outlined" onClick={() => router.back()}>
+          Cancel
+        </Button>
+      </Row>
+    </Container>
+  );
+}
