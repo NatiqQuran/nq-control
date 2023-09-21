f82e1c0d82a5b1a5058da0012657850234373653
ahadvl <ahamech2@gmail.com>
2023-09-21 12:45:30 +0330

    theme change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,21 @@ import "../styles/style.css";
 import { Theme } from "@yakad/ui";
 
 export default function Layout({ children }: any) {
-    return (
-        <html>
-            <head>
-                <meta charSet="utf-8" />
-                <link rel="icon" href="favicon.ico" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <meta name="theme-color" content="#222222" />
-                <meta name="description" content="Natiq Control Panel" />
-                <link rel="apple-touch-icon" href="logo192.png" />
-                <link rel="manifest" href="manifest.json" />
-                <title>Natiq Panel</title>
-            </head>
-            <body>
-                <Theme mode="dark">{children}</Theme>
-            </body>
-        </html>
-    );
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#222222" />
+        <meta name="description" content="Natiq Control Panel" />
+        <link rel="apple-touch-icon" href="logo192.png" />
+        <link rel="manifest" href="manifest.json" />
+        <title>Natiq Panel</title>
+      </head>
+      <body>
+        <Theme mode="system">{children}</Theme>
+      </body>
+    </html>
+  );
 }
