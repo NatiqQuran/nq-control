82e0d4f275f697d53d5441b3f86ea499ddfcf991
Al-Abd <nexnema@gmail.com>
2023-09-24 21:54:47 +0330

    Surahs Table

--- app/panel/surah/list/page.tsx
+++ app/panel/surah/list/page.tsx
@@ -1,18 +1,21 @@
-import { Container } from "@yakad/ui";
-import { Xtable } from "@yakad/x";
+import {
+    Container,
+    Button,
+    Table,
+    Tbody,
+    Td,
+    Th,
+    Thead,
+    Tr,
+    Row,
+} from "@yakad/ui";
+import Link from "next/link";
 
 interface SimpleSurah {
     name: string;
     period: string | null;
     number: number;
     number_of_ayahs: number;
-    uuid: string;
-}
-
-interface SurahTableHeader {
-    name: string;
-    number: string;
-    number_of_ayahs: string;
     uuid: string;
 }
 
@@ -24,16 +27,50 @@ async function getSurahsList(): Promise<SimpleSurah[]> {
 
 export default async function Page() {
     const surahsList = await getSurahsList();
-    const tableData: SurahTableHeader = {
-        name: "Surah Name",
-        number: "Surah Number",
-        number_of_ayahs: "Number of Ayahs",
-        uuid: "Surah uuid",
-    };
+
     return (
         <Container>
             <h1>Surah List</h1>
-            <Xtable head={tableData} data={surahsList} />
+            <Table>
+                <Thead>
+                    <Tr>
+                        <Th>Surah Number</Th>
+                        <Th>Surah Name</Th>
+                        <Th>Number of Ayyahs</Th>
+                        <Th>Period</Th>
+                        <Th>More</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {surahsList.map((item) => (
+                        <Tr>
+                            <Td>{item.number}</Td>
+                            <Td>{item.name}</Td>
+                            <Td>{item.number_of_ayahs}</Td>
+                            <Td>{item.period}</Td>
+                            <Td>
+                                <Row>
+                                    <Link href={"/panel/surah/" + item.uuid}>
+                                        <Button size="small" variant="link">
+                                            View
+                                        </Button>
+                                    </Link>
+                                    <Link
+                                        href={"/panel/surah/edit/" + item.uuid}
+                                    >
+                                        <Button size="small" variant="link">
+                                            Edit
+                                        </Button>
+                                    </Link>
+                                    <Button size="small" variant="link">
+                                        Delete
+                                    </Button>
+                                </Row>
+                            </Td>
+                        </Tr>
+                    ))}
+                </Tbody>
+            </Table>
         </Container>
     );
 }
