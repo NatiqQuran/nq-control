9f5c79b66eb995faaeeb338045c5657348ba6abf
ahadvl <ahamech2@gmail.com>
2023-09-29 20:51:19 +0330

    panel redirect to overview

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -1,5 +1,6 @@
 import { Container, Button, Row } from "@yakad/ui";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
-    return <Container></Container>;
+    return redirect("/panel/overview");
 }
