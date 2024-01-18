e69e58b05e659a8a3344931ed749737f206f6431
AzerothA <telifesite@gmail.com>
2024-01-18 16:53:20 +0330

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,22 +3,18 @@ import { Theme } from "@yakad/ui";
 
 export const runtime = "edge";
 
+export const metadata = {
+    title: "Natiq Panel",
+    description: "Natiq Control Panel",
+    themeColor: "#222222",
+    manifest: "manifest.json",
+    viewport: "width=device-width, initial-scale=1",
+    icons: "favicon.ico",
+}
+
 export default function Layout({ children }: any) {
     return (
         <html>
-            <head>
-                <meta charSet="utf-8" />
-                <link rel="icon" href="favicon.ico" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <meta name="theme-color" content="#222222" />
-                <meta name="description" content="Natiq Control Panel" />
-                <link rel="apple-touch-icon" href="logo192.png" />
-                <link rel="manifest" href="manifest.json" />
-                <title>Natiq Panel</title>
-            </head>
             <body>
                 <Theme mode="system">{children}</Theme>
             </body>
